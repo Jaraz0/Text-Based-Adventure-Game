3915ada862cfc58bdb668f55fa30adaf032d0d86
cartercasper <49322401+cartercasper@users.noreply.github.com>
2019-04-05 12:47:19 -0700

    Just the first test
    I changed it

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class test {
     public static void main (String [] args){
-        System.out.println("GitHub Works");
+        System.out.println("Works");
 
   }
 }
