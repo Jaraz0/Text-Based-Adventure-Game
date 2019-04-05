3ef643d879cde06b167f4fa00ed7d0e0be38c1b6
cartercasper <49322401+cartercasper@users.noreply.github.com>
2019-04-05 12:36:48 -0700

    Just the first test

--- src/test.java
+++ src/test.java
@@ -2,12 +2,7 @@ import java.util.ArrayList;
 
 public class test {
     public static void main (String [] args){
-        int x = 1;
-while (x < 100){
-    x*=2;
-}
-
-System.out.println(x);
+        System.out.println("Testing the GitHub thing");
 
   }
 }
