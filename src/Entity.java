7598ee98f529aa2a27c030f9cb86ae129ccf2cf0
cartercasper <49322401+cartercasper@users.noreply.github.com>
2019-05-08 18:57:29 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

--- src/Entity.java
+++ src/Entity.java
@@ -6,18 +6,85 @@
  */
 public abstract class Entity {
 
-    int health = 100;
-    int damage, attack ;
-    double armor = 0.0;
+   private int health = 100;
+   private int damage, attack, experience;
+   private double armor = 0.0;
+   private String name;
 
 
-    public Entity (int eHealth, int eAttack, double eArmor, String)
+    public Entity (int eHealth, int eAttack, double eArmor, String eName, int exp)
     {
         health = eHealth;
         attack = eAttack;
         armor = eArmor;
+        name = eName;
+        experience = exp;
     }
 
+    /**
+     * Sets Entity's health to specified amount.
+     *
+     * @param  h -- the amount the health is being set to.
+     *
+     */
+    public void setHealth(int h)
+    {
+        health = h;
+    }
+
+    /**
+     * Sets Entity's attack to specified amount.
+     *
+     * @param  a -- the amount the attack is being set to.
+     *
+     */
+    public void setAttack(int a)
+    {
+        attack = a;
+    }
+
+    /**
+     * Sets Entity's experience to specified amount.
+     *
+     * @param  exp -- the amount the experience is being set to.
+     *
+     */
+    public void setExperience(int exp)
+    {
+        experience = exp;
+    }
+
+    /**
+     * Returns the value stored in the Entity's experience variable.
+     *
+     * @return the experience of the Entity.
+     */
+    public int getExperience()
+    {
+        return experience;
+    }
+
+
+    /**
+     * Sets Entity's armor to specified amount.
+     *
+     * @param  a -- the amount the armor is being set to.
+     *
+     */
+    public void setArmor(double a)
+    {
+        armor = a;
+    }
+
+    /**
+     * Returns the string stored in the entity's name variable.
+     *
+     * @return the name of the entity.
+     */
+    public String getName()
+    {
+        return name;
+    }
 
     public int getHealth() {
         return health;
--- src/Player.java
+++ src/Player.java
@@ -7,22 +7,52 @@
 public class Player extends Entity {
 
     int level;
-    int experience;
-    String name;
+
 
     /**
      * Constructor for player objects.
      *
-     * @param  n-- the name the user wants to give their player object.
+     * @param health -- the base health for the player object.
+     * @param attack -- the base attack for the player object.
+     * @param armor -- the percentage of damage reduction(in a decimal) the character receives.
+     * @param  name -- the name the user wants to give their player object.
      *
      */
-    public Player(String n)
+    public Player(int health, int attack, double armor, String name)
     {
-        name = n;
+        super(health, attack, armor, name, 0); //constructor of Entity superclass
         level = 1;
-        experience = 0;
     }
 
+    /**
+     * Sets Entity's level based on the value of experience.
+     *
+     */
+    public void levelUp()
+    {
+       /* if(experience >= 3) level = 2;
+        if(experience >= 6) level = 3;
+        if(experience >= 9) level = 4;
+        if(experience >= 12) level = 5;
+        if(experience >= 15) level = 6;
+        if(experience >= 18) level = 7;
+        if(experience >= 21) level = 8;
+        if(experience >= 24) level = 9;
+        if(experience >= 27) level = 10;
+        else level = 1;
+        */
+    }
+
+    /**
+     * Adds a specified amount to experience.
+     *
+     * @param  exp the amount to be added to experience
+     *
+     */
+    public void addExperience(int exp)
+    {
+        super.setExperience(super.getExperience()+exp);
+    }
 }
 
 
