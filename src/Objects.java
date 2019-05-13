f5ea31b4bdb59c14b9e076cf8bc4656ee66c1153
alec01134 <alecrhenry@gmail.com>
2019-05-13 11:33:59 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/workspace.xml

---
+++ src/Objects.java
@@ -0,0 +1,70 @@
+/**
+ * Write a description of class here.
+ *
+ * @author (Jordan Mraz, Alec Henry, Michael Gery, Carter Casper)
+ * @version (5/8/2019)
+ */
+public class Objects {
+
+    private String objectName;
+    private double defenseStat = 0;
+    private int attackStat = 0;
+
+
+
+    public Objects(String objName,int atkstat, double defstat)
+    {
+         objectName = objName;
+        attackStat = atkstat;
+        defenseStat = defstat;
+
+        /**
+         * Constructor for Objects objects.
+         *
+         * @param objectName
+         * @param defenseStat
+         * @param attackStat
+         */
+
+    }
+
+
+
+
+    public double getArmor()
+    {
+        return defenseStat;
+    }
+
+    /**
+     * Returns the value of the armor object
+     *
+     * @return -- the amount that helps reduce an attack.
+     *
+     */
+
+    public double getWeapon()
+    {
+        return attackStat;
+    }
+
+    /**
+     * Sets Entity's health to specified amount.
+     *
+     * @return  attackStat -- the amount of hitpoints that is taken away off an entity.
+     *
+     */
+
+    public String getName()
+    {
+        return objectName;
+    }
+
+    /**
+     * Sets Entity's health to specified amount.
+     *
+     * @return  objectName -- the name of the object.
+     *
+     */
+
+}
